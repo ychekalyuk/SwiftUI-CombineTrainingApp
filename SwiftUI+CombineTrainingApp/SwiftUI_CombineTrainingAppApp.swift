3ebaa85d230cdd8979d6ed43827b4db24bc20e80
Yuri Chekalyuk <y.chekalyuk@dirion.biz>
2023-03-10 00:39:15 +0300

    Initial Commit

---
+++ SwiftUI+CombineTrainingApp/SwiftUI_CombineTrainingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CombineTrainingAppApp.swift
+//  SwiftUI+CombineTrainingApp
+//
+//  Created by Юрий Альт on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CombineTrainingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
